3ba173bc736133029c2495eafbf592f28a57205e
thebluebsss <167521513+thebluebsss@users.noreply.github.com>
2025-12-23 16:44:47 +0700

    testdeploy

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "public", // Output vào public thay vì dist
+    emptyOutDir: true, // Xóa folder public trước khi build
+  },
+});
