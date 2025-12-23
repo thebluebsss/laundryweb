ae97da2a00a6018602b0692f008c6fe60493c52d
thebluebsss <167521513+thebluebsss@users.noreply.github.com>
2025-12-23 18:40:51 +0700

    testdeploy

--- be/server.js
+++ be/server.js
@@ -17,6 +17,14 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(
+  cors({
+    origin: ["https://my-laundry-web.web.app", "http://localhost:3001"], // Cho phép các domain này
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 const JWT_SECRET = process.env.JWT_SECRET || "your-secret-key-change-this";
 
